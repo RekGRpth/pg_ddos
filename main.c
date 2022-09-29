233c6988134675fa3c5faeac134b15dc4fd0a39f
Georgy Shelkovy <g.shelkovy@arenadata.io>
2022-09-29 14:33:17 +0600

    up

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ static void ddos_finish(ddos_t *ddos) {
     free(ddos);
 }
 
-static void ddos_reset(ddos_t *ddos);
+static void ddos_on_poll(uv_poll_t *handle, int status, int events);
 
 static void ddos_error(ddos_t *ddos, PGresult *res) {
     //char *message = PQresultErrorMessage(res); // char *PQresultErrorMessage(const PGresult *res)
@@ -54,6 +54,14 @@ static void ddos_error(ddos_t *ddos, PGresult *res) {
     //request_t *request = ddos->request;
 //    DEBUG("sqlstate=%s\n", sqlstate);
     //if (ddos_code_body(request, ddos_sqlstate_to_code(sqlstate), message, strlen(message))) FATAL("ddos_code_body\n");
+}
+
+static void ddos_select(ddos_t *ddos) {
+//    DEBUG("postgres=%p\n", postgres);
+    //if (!PQisnonblocking(postgres->conn) && PQsetnonblocking(postgres->conn, 1)) FATAL("PQsetnonblocking:%s", PQerrorMessage(postgres->conn)); // int PQisnonblocking(const PGconn *conn); int PQsetnonblocking(PGconn *conn, int arg); char *PQerrorMessage(const PGconn *conn)
+    if (!PQsendQuery(ddos->conn, "select now()")) { ERROR("!PQsendQuery and %s", PQerrorMessage(ddos->conn)); ddos_finish(ddos); return; }// int PQsendQuery(PGconn *conn, const char *command); char *PQerrorMessage(const PGconn *conn)
+    int error;
+    if ((error = uv_poll_start(&ddos->poll, UV_WRITABLE, ddos_on_poll))) { ERROR("uv_poll_start = %s", uv_strerror(error)); ddos_finish(ddos); return; } // int uv_poll_start(uv_poll_t* handle, int events, uv_poll_cb cb)
 }
 
 static void ddos_success(ddos_t *ddos, PGresult *res) {
@@ -90,10 +98,24 @@ static void ddos_success(ddos_t *ddos, PGresult *res) {
 //    if (ddos_info_body(request, PQgetvalue(res, 0, info), PQgetlength(res, 0, info), PQgetvalue(res, 0, body), PQgetlength(res, 0, body))) FATAL("ddos_info_body\n");
 }
 
+static void ddos_reset(ddos_t *ddos) {
+//    DEBUG("ddos=%p, ddos->request=%p\n", ddos, ddos->request);
+    int error;
+    if (uv_is_active((uv_handle_t *)&ddos->poll)) if ((error = uv_poll_stop(&ddos->poll))) { ERROR("uv_poll_stop = %s", uv_strerror(error)); ddos_finish(ddos); return; } // int uv_is_active(const uv_handle_t* handle); int uv_poll_stop(uv_poll_t* poll)
+    //if (ddos->request) if (request_push(ddos->request)) FATAL("request_push\n");
+    //ddos->request = NULL;
+    //pointer_remove(&ddos->server_pointer);
+//    PQfinish(ddos->conn);
+//    if ((error = ddos_connect(ddos->poll.loop, ddos))) { FATAL("ddos_connect\n"); ddos_reset(ddos); return error; }
+    if (!PQresetStart(ddos->conn)) { ERROR("!PQresetStart and %s", PQerrorMessage(ddos->conn)); ddos_finish(ddos); return; } // int PQresetStart(PGconn *conn);
+    if ((ddos->poll.io_watcher.fd = PQsocket(ddos->conn)) < 0) { ERROR("PQsocket < 0"); ddos_finish(ddos); ddos_finish(ddos); return; }
+    if ((error = uv_poll_start(&ddos->poll, UV_WRITABLE, ddos_on_poll))) { ERROR("uv_poll_start = %s", uv_strerror(error)); ddos_finish(ddos); return; } // int uv_poll_start(uv_poll_t* handle, int events, uv_poll_cb cb)
+}
+
 static void ddos_on_poll(uv_poll_t *handle, int status, int events) { // void (*uv_poll_cb)(uv_poll_t* handle, int status, int events)
 //    DEBUG("handle=%p, status=%i, events=%i\n", handle, status, events);
     ddos_t *ddos = handle->data;
-    if (status) { ERROR("status = %i", status); ddos_reset(ddos); return; }
+    if (status) { ERROR("status = %i", status); ddos_finish(ddos); return; }
     if (PQsocket(ddos->conn) < 0) { ERROR("PQsocket < 0"); ddos_reset(ddos); return; } // int PQsocket(const PGconn *conn)
 //    DEBUG("PQstatus(ddos->conn)=%i\n", PQstatus(ddos->conn));
     int error;
@@ -128,23 +150,9 @@ static void ddos_on_poll(uv_poll_t *handle, int status, int events) { // void (*
     //}
 }
 
-static void ddos_reset(ddos_t *ddos) {
-//    DEBUG("ddos=%p, ddos->request=%p\n", ddos, ddos->request);
-    int error;
-    if (uv_is_active((uv_handle_t *)&ddos->poll)) if ((error = uv_poll_stop(&ddos->poll))) { ERROR("uv_poll_stop = %s", uv_strerror(error)); ddos_finish(ddos); return; } // int uv_is_active(const uv_handle_t* handle); int uv_poll_stop(uv_poll_t* poll)
-    //if (ddos->request) if (request_push(ddos->request)) FATAL("request_push\n");
-    //ddos->request = NULL;
-    //pointer_remove(&ddos->server_pointer);
-//    PQfinish(ddos->conn);
-//    if ((error = ddos_connect(ddos->poll.loop, ddos))) { FATAL("ddos_connect\n"); ddos_reset(ddos); return error; }
-    if (!PQresetStart(ddos->conn)) { ERROR("!PQresetStart and %s", PQerrorMessage(ddos->conn)); ddos_finish(ddos); return; } // int PQresetStart(PGconn *conn);
-    if ((ddos->poll.io_watcher.fd = PQsocket(ddos->conn)) < 0) { ERROR("PQsocket < 0"); ddos_finish(ddos); ddos_finish(ddos); return; }
-    if ((error = uv_poll_start(&ddos->poll, UV_WRITABLE, ddos_on_poll))) { ERROR("uv_poll_start = %s", uv_strerror(error)); ddos_finish(ddos); return; } // int uv_poll_start(uv_poll_t* handle, int events, uv_poll_cb cb)
-}
-
 static void ddos_on_start(void *arg) { // void (*uv_thread_cb)(void* arg)
     //DEBUG("arg");
-    uv_loop_t *loop = arg;
+    //uv_loop_t *loop = arg;
     char *conninfo = getenv("DDOS_ddos_CONNINFO"); // char *getenv(const char *name)
     if (!conninfo) conninfo = "postgresql://localhost?application_name=ddos";
     char *ddos_ddos_count = getenv("DDOS_ddos_COUNT"); // char *getenv(const char *name);
@@ -154,6 +162,8 @@ static void ddos_on_start(void *arg) { // void (*uv_thread_cb)(void* arg)
     ddos_t *ddos;
     //uv_os_sock_t ddos_sock;
     int error;
+    uv_loop_t loop;
+    if ((error = uv_loop_init(&loop))) { FATAL("uv_loop_init = %s", uv_strerror(error)); return; } // int uv_loop_init(uv_loop_t* loop)
     for (int i = 0; i < count; i++) {
         if (!(ddos = malloc(sizeof(*ddos)))) { ERROR("!malloc"); continue; }
         if (PQstatus(ddos->conn = PQconnectStart(conninfo)) == CONNECTION_BAD) { ERROR("PQstatus = CONNECTION_BAD and %s", PQerrorMessage(ddos->conn)); ddos_finish(ddos); continue; }
@@ -161,10 +171,12 @@ static void ddos_on_start(void *arg) { // void (*uv_thread_cb)(void* arg)
         //if ((ddos_sock = PQsocket(ddos->conn)) < 0) { ERROR("PQsocket < 0"); ddos_finish(ddos); continue; }
         if ((ddos->poll.io_watcher.fd = PQsocket(ddos->conn)) < 0) { ERROR("PQsocket < 0"); ddos_finish(ddos); ddos_finish(ddos); continue; }
         (void)PQsetNoticeProcessor(ddos->conn, ddos_notice_processor, ddos);
-        if ((error = uv_poll_init_socket(loop, &ddos->poll, ddos->poll.io_watcher.fd))) { ERROR("uv_poll_init_socket = %s", uv_strerror(error)); ddos_finish(ddos); continue; } // int uv_poll_init_socket(uv_loop_t* loop, uv_poll_t* handle, uv_os_sock_t socket)
+        if ((error = uv_poll_init_socket(&loop, &ddos->poll, ddos->poll.io_watcher.fd))) { ERROR("uv_poll_init_socket = %s", uv_strerror(error)); ddos_finish(ddos); continue; } // int uv_poll_init_socket(uv_loop_t* loop, uv_poll_t* handle, uv_os_sock_t socket)
         ddos->poll.data = ddos;
         if ((error = uv_poll_start(&ddos->poll, UV_WRITABLE, ddos_on_poll))) { ERROR("uv_poll_start = %s", uv_strerror(error)); ddos_finish(ddos); continue; } // int uv_poll_start(uv_poll_t* handle, int events, uv_poll_cb cb)
     }
+    if ((error = uv_run(&loop, UV_RUN_DEFAULT))) { FATAL("uv_run = %s", uv_strerror(error)); return; } // int uv_run(uv_loop_t* loop, uv_run_mode mode)
+//    if ((error = uv_loop_close(&loop))) { FATAL("uv_loop_close = %s", uv_strerror(error)); return; } // int uv_loop_close(uv_loop_t* loop)
 }
 
 int main(int argc, char **argv) {
@@ -195,7 +207,7 @@ int main(int argc, char **argv) {
         for (int i = 0; i < thread_count; i++) if ((error = uv_thread_create(&tid[i], ddos_on_start, (void *)&loop))) { FATAL("uv_thread_create = %s", uv_strerror(error)); return error; } // int uv_thread_create(uv_thread_t* tid, uv_thread_cb entry, void* arg)
         for (int i = 0; i < thread_count; i++) if ((error = uv_thread_join(&tid[i]))) { FATAL("uv_thread_join = %s", uv_strerror(error)); return error; } // int uv_thread_join(uv_thread_t *tid)
     }
-    if ((error = uv_run(&loop, UV_RUN_DEFAULT))) { FATAL("uv_run = %s", uv_strerror(error)); return error; } // int uv_run(uv_loop_t* loop, uv_run_mode mode)
-    if ((error = uv_loop_close(&loop))) { FATAL("uv_loop_close = %s", uv_strerror(error)); return error; } // int uv_loop_close(uv_loop_t* loop)
+//    if ((error = uv_run(&loop, UV_RUN_DEFAULT))) { FATAL("uv_run = %s", uv_strerror(error)); return error; } // int uv_run(uv_loop_t* loop, uv_run_mode mode)
+//    if ((error = uv_loop_close(&loop))) { FATAL("uv_loop_close = %s", uv_strerror(error)); return error; } // int uv_loop_close(uv_loop_t* loop)
     return error;
 }
